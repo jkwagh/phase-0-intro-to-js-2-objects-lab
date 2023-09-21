d864fcc79b5d4e9da36ece9c43b3f68052927698
Jeremiah <kwagh.jeremiah@gmail.com>
2023-09-21 17:47:43 -0400

    complete lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    Name: "", 
+    streetAddress: "" ,
+};
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+console.log(employee);
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const copytOfEmployee = {...employee}
+    delete copytOfEmployee[key];
+    return copytOfEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
